40320f739474ca4384885e42616c8203f52817db
Timothy Korea <TimothyKorea25@gmail.com>
2026-05-14 18:27:05 -0700

    Add search overlay interaction states and toggle behavior

--- js/app.js
+++ js/app.js
@@ -27,3 +27,15 @@ function hideMenu() {
     body.classList.remove('menu-open');
     overlay.classList.remove('is-active');
 }
+
+const searchIcon = document.querySelector('.js-search-icon');
+const searchOverlay = document.querySelector('.js-search-overlay');
+const searchOverlayClose = document.querySelector('.js-overlay-close');
+
+searchIcon.addEventListener('click', () => {
+    searchOverlay.classList.add('active');
+})
+
+searchOverlayClose.addEventListener('click', () => {
+    searchOverlay.classList.remove('active');
+})
