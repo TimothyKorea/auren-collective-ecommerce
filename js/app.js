669400b13b2854e9f5e894d43eb360ffa0f42697
Timothy Korea <TimothyKorea25@gmail.com>
2026-05-21 20:15:29 -0700

    feat(best-sellers): render dynamic product grid with responsive layout styling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
+import {products} from './data.js';
+
+function renderProducts(productArray, container) {
+    container.innerHTML = "";
+
+    productArray.forEach(product => {
+        container.innerHTML += `
+        <div class="product-card__container">
+                <div class="product-card__img-wrapper">
+                    <img class="product-card__img" src="${product.img}" alt="${product.imageAlt}">
+                </div>
+
+                <div class="product-card__info-wrapper">
+                    <div class="product-card__info">
+                        <span class="product-card__seasonal-tag">${product.seasonalTag}</span>
+                        <h3 class="product-card__title">${product.title}</h3>
+                        <div class="product-card__meta-row">
+                            <div class="product-card__meta">
+                                <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="rgb(160, 25, 55)"><path d="M12 2.5l2.94 5.96 6.58.96-4.76 4.64 1.12 6.56L12 17.77 6.12 20.62l1.12-6.56L2.48 9.42l6.58-.96L12 2.5z"/></svg></span>
+                                <span class="product-card__rating">${product.rating}</span>
+                            </div>
+                            <p class="product-card__colors">${product.colors}</p>
+                        </div>
+                    </div>
+
+                    <span class="product-card__price">$${product.price / 100}</span>
+                </div>
+            </div>
+        `
+    })
+}
+
+const bestSeller = products.filter(eachProduct => eachProduct.isBestSeller === true);
+renderProducts(bestSeller, document.querySelector('.best-sellers__product-grid'));
+
+
 const mobileMenuBtn = document.querySelector('.js-mobile-menu-toggle');
 const mobileMenu = document.querySelector('.js-mobile-menu');
 
---
+++ js/data.js
@@ -0,0 +1,121 @@
+export const products = [
+    {
+        id: 1,
+        title: "Sweatshirt",
+        category: "Men",
+        img: "assets/images/best-seller1.png",
+        imageAlt: "charcoal sweatshirt flat lay",
+        wishlistHeart:"",
+        seasonalTag: "[ 01 / SS-0421 ]",
+        price: 48000,
+        rating: 4.8, 
+        colors: "3 Colors",
+        sizes: ["XS", "S", "M", "L", "XL"],
+        isBestSeller: true
+    },
+
+    {
+        id: 2,
+        title: "Leather Bag",
+        category: "Women",
+        img: "assets/images/bag-img.png",
+        imageAlt: "black leather bag",
+        wishlistHeart:"",
+        seasonalTag: "[ 02 / SS-0322 ]",
+        price: 20747,
+        rating: 4.9, 
+        colors: "3 Colors",
+        sizes: ["XS", "S", "M", "L", "XL"],
+        isBestSeller: true
+    },
+
+    {
+        id: 3,
+        title: "Wide-leg Trousers",
+        category: "Women",
+        img: "assets/images/best-seller3.png",
+        imageAlt: "navy wide leg trousers",
+        wishlistHeart:"",
+        seasonalTag: "[ 03 / SS-0512 ]",
+        price: 18791,
+        rating: 4.6, 
+        colors: "3 Colors",
+        sizes: ["XS", "S", "M", "L", "XL"],
+        isBestSeller: true
+    },
+
+    {
+        id: 4,
+        title: "Black Shirt",
+        category: "Men",
+        img: "assets/images/best-seller4.png",
+        imageAlt: "black shirt on wooden hanger",
+        wishlistHeart:"",
+        seasonalTag: "[ 04 / SS-0210 ]",
+        price: 18500,
+        rating: 4.9, 
+        colors: "3 Colors",
+        sizes: ["XS", "S", "M", "L", "XL"],
+        isBestSeller: true
+    },
+
+    {
+        id: 5,
+        title: "Indigo Scarf",
+        category: "Women",
+        img: "assets/images/best-seller5.png",
+        imageAlt: "indigo scarf on dark backdrop",
+        wishlistHeart:"",
+        seasonalTag: "[ 05 / SS-0701 ]",
+        price: 5799,
+        rating: 4.7, 
+        colors: "3 Colors",
+        sizes: ["XS", "S", "M", "L", "XL"],
+        isBestSeller: true
+    },
+
+    {
+        id: 6,
+        title: "Necktie",
+        category: "Men",
+        img: "assets/images/best-seller6.png",
+        imageAlt: "black tie with bird embroidery",
+        wishlistHeart:"",
+        seasonalTag: "[ 09 / SS-0422 ]",
+        price: 24800,
+        rating: 4.8, 
+        colors: "3 Colors",
+        sizes: ["XS", "S", "M", "L", "XL"],
+        isBestSeller: true
+    },
+
+    {
+        id: 7,
+        title: "Wool Coat",
+        category: "Women",
+        img: "assets/images/best-seller7.png",
+        imageAlt: "wool coat with collar",
+        wishlistHeart:"",
+        seasonalTag: "[ 07 / SS-0513 ]",
+        price: 72000,
+        rating: 4.6, 
+        colors: "3 Colors",
+        sizes: ["XS", "S", "M", "L", "XL"],
+        isBestSeller: true
+    },
+
+    {
+        id: 8,
+        title: "Leather Brogues",
+        category: "Men",
+        img: "assets/images/best-seller8.png",
+        imageAlt: "Elegant black leather brogues in focus",
+        wishlistHeart:"",
+        seasonalTag: "[ 08 / SS-0323 ]",
+        price: 35000,
+        rating: 4.7, 
+        colors: "3 Colors",
+        sizes: ["XS", "S", "M", "L", "XL"],
+        isBestSeller: true
+    }
+]
